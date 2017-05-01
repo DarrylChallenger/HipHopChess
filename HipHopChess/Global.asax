bfc932f14d9bea3c89bf7bb5df0de715e29182d4
Darryl.Challenger <Darryl.Challenger@gmail.com>
2017-05-01 17:21:02 -0400

    Add project files.

---
+++ HipHopChess/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="HipHopChess.MvcApplication" Language="C#" %>
---
+++ HipHopChess/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HipHopChess.Startup))]
+namespace HipHopChess
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
